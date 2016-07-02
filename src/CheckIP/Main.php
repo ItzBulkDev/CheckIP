05bcdddaaea4513b82f6dfa7d8ee6b319b46d9de
Pogogo007 <Francisd246@hotmail.com>
2016-07-02 09:01:07 -0400

    Update Main.php

--- src/CheckIP/Main.php
+++ src/CheckIP/Main.php
@@ -1,3 +1,13 @@
 <?php
  
 Namespace CheckIP;
+
+use pocketmine\Player;
+use pocketmine\Server;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\command\ConsoleCommandSender;
+use pocketmine\event\Listener;
+use pocketmine\plugin\PluginBase;
+
+class Main extends PluginBase implements Listenener{
