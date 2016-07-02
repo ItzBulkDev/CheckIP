e13ad97389f40435aa7fec4bf48c8aa3f4cbc51a
Pogogo007 <Francisd246@hotmail.com>
2016-07-02 19:57:34 -0400

    Update Main.php

--- src/CheckIP/Main.php
+++ src/CheckIP/Main.php
@@ -14,18 +14,21 @@ class Main extends PluginBase{
 
 public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
          if(strtolower($cmd->getName()) === "checkip"){
-          $name = $args[0];
+         if($sender->hasPermission("checkip.command"){
+            
                 
-                   
-                  
-      
-                      if(!isset($args[0])){           
+             if(!isset($args[0])){           
             $sender->sendMessage("§cUsage /CheckIP <player>");
         
-
-if(strtolower($args[0]) == $name){
+         }
+      
+        if(strtolower($args[0]) == $name){
                $target = $this->getServer()->getPlayer($name);
                 if($sender instanceof Player){
                $ip = $target->getAddress();
                 $playername = $target->getName();
                 $sender->sendMessage("§AShowing ".$playername."’s IP §9".$ip."");
+                      }
+               }
+       }
+}
