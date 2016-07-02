10d18f690534a0c1219e50935ab33fb813596dae
Pogogo007 <Francisd246@hotmail.com>
2016-07-02 09:30:26 -0400

    Update Main.php

--- src/CheckIP/Main.php
+++ src/CheckIP/Main.php
@@ -8,6 +8,7 @@ use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\command\ConsoleCommandSender;
 use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat;
 
 class Main extends PluginBase{
 
