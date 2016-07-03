921173c1d66f745bddfae6b8306b8a8754e28dcd
Pogogo007 <Francisd246@hotmail.com>
2016-07-02 20:18:41 -0400

    Update Main.php

--- src/CheckIP/Main.php
+++ src/CheckIP/Main.php
@@ -12,24 +12,3 @@ use pocketmine\utils\TextFormat;
 
 class Main extends PluginBase{
 
-public function onCommand(CommandSender $sender, Command $command, $label, array $args){
-		if(strtolower($command->getName('checkip'))){
-
-         if($sender->hasPermission("checkip.command"){
-            
-                
-             if(!isset($args[0])){           
-            $sender->sendMessage("§cUsage /CheckIP <player>");
-        
-         }
-      
-        if(strtolower($args[0]) == $name){
-               $target = $this->getServer()->getPlayer($name);
-                if($sender instanceof Player){
-               $ip = $target->getAddress();
-                $playername = $target->getName();
-                $sender->sendMessage("§AShowing ".$playername."’s IP §9".$ip."");
-                      }
-               }
-       }
-}
