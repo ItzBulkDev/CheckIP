b6b87dda997519b469e8ce66f8ed0b535ef04fe5
SavionLegendZzz <saviondeavault@gmail.com>
2016-07-10 13:19:38 -0400

    That's better
    I have not tested this yet so i don't know if it will work :+1:

--- src/CheckIP/Main.php
+++ src/CheckIP/Main.php
@@ -4,32 +4,41 @@ namespace CheckIP;
 
 use pocketmine\Player;
 use pocketmine\Server;
+
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\command\ConsoleCommandSender;
+
 use pocketmine\plugin\PluginBase;
-use pocketmine\utils\TextFormat;
-use pocketmine\Server;
+
+use pocketmine\utils\TextFormat as TF;
 
 class Main extends PluginBase{
 
-public function onEnable(){
-        $this->getServer()->getPluginManager()->registerEvents($this, $this);
-}
+  public function onEnable(){
+    $this->getServer()->getPluginManager()->registerEvents($this,$this);
+    $this->getServer()->getLogger()->info(TF::GOLD."Enabled!");
+  }
 
-public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
-        if($cmd->getName() == "checkip"){
-           if(isset($args[0])){
-              $name = $args[0];
-                $player = $this->getServer()->getPlayer($name);
-             $ip = $player->getAddress();
-                 $playername = $player->getName();     
-              $sender->sendMessage("§9".$playername."'s §aIP is §c".$ip."");
+  public function onDisable(){
+    $this->getServer()->getLogger()->info(TF::RED."Disabled!");
+  }
 
-               }else{
-                  $sender->sendMessage("§cUse /CheckIp <player>");
-
-                 }
-         }
-     }  
+  public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
+    if(strtolower($cmd->getName()) === "ip"){
+      if(!($sender->hasPermission("check.ip.perm"))){
+        $sender->sendMessage(TF::RED."You don't have permission to use the /ip command!");
+        return;
+      }
+      if(isset($args[0])){
+        $target = $this->getServer()->getPlayer($args[0]);
+        if($target === null){
+          $sender->sendMessage(TF::RED."That player isn't online!");
+        }else{
+          $ip = $target->getAddress();
+          $sender->sendMessage(TF::GREEN.$target->getName()."'s IP is ".$ip."!");
+        }
+      }
+    }
+  }
 }
